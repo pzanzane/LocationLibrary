4ef0ea95a4692fb2895fd82bd431f9eefb699473
Pankaj Zanzane <pankaj@pankaj-DL-IPMSB-H61.(none)>
2013-08-07 13:06:44 +0530

    Demo Activity added for test

--- src/com/LocationLibrary/DemoActivity.java
+++ src/com/LocationLibrary/DemoActivity.java
@@ -1,21 +1,38 @@
 package com.LocationLibrary;
 
+import com.LocationLibrary.db.model.LocationsModel;
 import com.google.android.gms.location.LocationRequest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class DemoActivity extends Activity {
+public class DemoActivity extends Activity implements OnClickListener{
 
-	LocationUtils utils;
+	private LocationUtils utils;
+	private Button button;
+	private TextView txtLattitude,txtLongitude;
+	private int invalideTimeInSeconds=180,intervalToFetchLocation=60;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+ 		setContentView(R.layout.activity_demo);
+		
+		button = (Button)findViewById(R.id.button1);
+		txtLattitude = (TextView)findViewById(R.id.txtLattitude);
+		txtLongitude = (TextView)findViewById(R.id.txtLongitude);
+		
+		button.setOnClickListener(this);
 		
 		utils = LocationUtils.getInstance();
 		LocationUtils.initializeLocations(getApplicationContext());
 		utils.startFetchingLocations(	getApplicationContext(),
-										60,
+		                             	intervalToFetchLocation,
 										LocationRequest.PRIORITY_HIGH_ACCURACY,
 										0);
 	}
@@ -26,5 +43,22 @@ public class DemoActivity extends Activity {
 		
 		utils.stopFetchingLocations();
 		utils.clearLocationsTable();
+	}
+
+	@Override
+	public void onClick(View v) { 
+		
+		LocationsModel model = utils.getLatestLocation(invalideTimeInSeconds);
+		
+		if(model!=null){
+			txtLattitude.setText(model.getLattitude()+"");
+			txtLongitude.setText(model.getLongitude()+"");
+		}else{
+			Toast.makeText(	getApplicationContext(),
+							"Location Not found " + invalideTimeInSeconds + " seconds prior to current time",
+							Toast.LENGTH_SHORT)
+					.show();
+		}
+		
 	}
 }
